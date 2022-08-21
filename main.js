4525bbaa7b5775da84889a244913031c499094e4
hollykaboaz <hollykaboaz@gmail.com>
2022-08-20 21:35:04 -0400

    Fixed add new term
    Changed styling of website and fixed adding new term system. Upon adding new style a button called "Add new button" is added and will later be given functionality"

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 function addNewBox() {
+
+  // Create new term box
   var newBox = document.createElement('span');
   newBox.classList.add('newTerm');
 
+  // create text field to put inside term box
   var termField = document.createElement('textArea');
-  // termField.setAttribute("data-role", "none");
   termField.classList.add("textField");
   termField.placeholder = "Term";
 
+  // create definition button for term box
+  var button = document.createElement('button');
+  button.classList.add('btn');
+  button.innerHTML = "Add a Definition";
+  button.classList.add('btnStyle');
 
-  var definitionField = document.createElement('INPUT');
-  definitionField.classList.add("definitionField");
 
-  // div.style.position = "relative";
-  // div.style.height = "200px";
-  // div.style.width = "200px";
-  // div.style.backgroundColor = "#312F2F";
-  newBox.appendChild(termField)
+
+
+
+
+
+
+  // append all created elements
+  newBox.appendChild(termField);
+  newBox.appendChild(button);
   document.body.appendChild(newBox);
+
 }
