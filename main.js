bcb5633da9846fe89fce3a867aae63a208d99fc6
hollykaboaz <hollykaboaz@gmail.com>
2022-08-23 01:14:25 -0400

    added code for eye

--- main.js
+++ main.js
@@ -6,13 +6,36 @@ function addNewBox() {
 
   // create text field to put inside term box
   var termField = document.createElement('textArea');
+  termField.setAttribute('id','insideBox');
   termField.classList.add("textField");
   termField.placeholder = "Term";
 
   // create understanding buttons inside term box
+  // var eyeButton = document.createElement('button');
+  // eyeButton.classList.add("transparentButton","eye");
+  // var eyeIcon = document.createElement('i');
+  // eyeButton.appendChild(eyeIcon);
+  // eyeIcon.classList.add("fa-solid", "fa-eye");
+
+  // create understanding buttons inside term box
   var goodButton = document.createElement('button');
+  goodButton.setAttribute('id','insideBox');
+  var goodIcon = document.createElement('i');
+  goodButton.appendChild(goodIcon);
+  goodIcon.classList.add("fa-solid", "fa-thumbs-up");
+
   var okButton = document.createElement('button');
+  okButton.setAttribute('id','insideBox');
+  var okIcon = document.createElement('i');
+  okButton.appendChild(okIcon);
+  okIcon.classList.add("fa-solid", "fa-thumbs-up");
+
+
   var badButton = document.createElement('button');
+  badButton.setAttribute('id','insideBox');
+  var badIcon = document.createElement('i');
+  badButton.appendChild(badIcon);
+  badIcon.classList.add("fa-solid", "fa-thumbs-down");
 
   // adding styling class to buttons
   goodButton.classList.add('btn','goodBtn');
@@ -20,27 +43,24 @@ function addNewBox() {
   badButton.classList.add('btn','badBtn');
 
 
-  // create definition button under term box
-  var button = document.createElement('button');
-  var iElement = document.createElement('i');
-  button.classList.add('transparentBtn');
-  iElement.classList.add('fa-solid','fa-pen');
-    button.appendChild(iElement);
-  // button.innerHTML = "Add a Definition";
-  // button.classList.add('btnStyle');
-
-
-
-
-
-
-
-
+  // // create definition button under term box
+  // var button = document.createElement('button');
+  // var iElement = document.createElement('i');
+  // button.classList.add('editButton');
+  // iElement.classList.add('fa-solid','fa-pen');
+  // iElement.on
+  //   button.appendChild(iElement);
+  // // button.innerHTML = "Add a Definition";
+  // // button.classList.add('btnStyle');
 
   // append all created elements
   newBox.appendChild(termField);
   newBox.append(goodButton, okButton, badButton);
-  newBox.appendChild(button);
+  // newBox.appendChild(button);
   document.body.appendChild(newBox);
 
 }
+document.getElementByClass('newTerm').onmouseover =
+     function() {
+         this.className='newactive';
+     };
